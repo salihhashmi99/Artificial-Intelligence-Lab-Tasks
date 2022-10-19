c932b55166d880142a9feea7fa4461ac94982964
Salih Tanveer Hashmi <82093715+salihhashmi99@users.noreply.github.com>
2022-10-19 09:54:53 -0700

    Create Pandas Library.py

---
+++ Pandas Library.py
@@ -0,0 +1,29 @@
+"""
+Write a Pandas program to select the rows where the number of attempts in the examination is
+greater than 2.
+Sample Python dictionary data and list labels:
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew',
+'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+Expected Output:
+Number of attempts in the examination is greater than 2:
+name score attempts qualify
+b Dima 9.0 3 no
+d James NaN 3 no
+f Michael 20.0 3 yes
+"""
+
+import pandas as pd
+import numpy as np
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew',
+'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df=pd.DataFrame(exam_data, index=labels)
+print("Total Attempts in the examination greater than 2 :")
+print(df[(df['attempts'] > 2)])
